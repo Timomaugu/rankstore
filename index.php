f7db1961751d0fc1b0cdc342eadad9f957c2658c
Timomaugu <timomaugu@gmail.com>
2022-11-07 11:56:02 +0300

    removed ads from index

--- index.php
+++ index.php
@@ -118,22 +118,7 @@
     </div>
     <div id="exp-text">Results = Average for a simulation of 1000 matches by the teams.</div>
     <script src="js/mainleague.js"></script>
-    <!-- SHOW ADSENSE ADS -->
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2356670002081614"
-     crossorigin="anonymous"></script>
-
-     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2356670002081614"
-     crossorigin="anonymous"></script>
-<!-- horizontal_ad -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-2356670002081614"
-     data-ad-slot="8468501700"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+    
 </body>
 
 </html>
